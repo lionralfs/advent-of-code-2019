309012e6aa49bac9081fa5ffe5bb5d8344e042c3
Lion Ralfs <lion.ralfs@gmail.com>
2019-12-01 14:29:19 +0100

    [day01] add part 2

--- day01/main.go
+++ day01/main.go
@@ -7,26 +7,61 @@ import (
 	"strconv"
 )
 
-func main() {
-	file, err := os.Open("./input.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
+type fuelCalcFn func(int) int
 
-	total := 0
+func calculateFuel(mass int) int {
+	return (mass / 3) - 2
+}
+
+func calculateFuelRec(mass int) int {
+	fuel := calculateFuel(mass)
+	if fuel <= 0 {
+		return 0
+	}
+	return fuel + calculateFuelRec(fuel)
+}
+
+func totalFuelCalculator(file *os.File, fn fuelCalcFn) int {
 	scanner := bufio.NewScanner(file)
+	total := 0
 	for scanner.Scan() {
-		asInt, err := strconv.Atoi(scanner.Text())
+		mass, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			panic(err)
 		}
-		total += (asInt / 3) - 2
+		total += fn(mass)
 	}
 
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Total: %v\n", total)
+	return total
+}
+
+func partOne() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	total := totalFuelCalculator(file, calculateFuel)
+
+	fmt.Printf("[Part1] Total: %v\n", total)
+}
+
+func partTwo() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	total := totalFuelCalculator(file, calculateFuelRec)
+
+	fmt.Printf("[Part2] Total: %v\n", total)
+}
+
+func main() {
+	partOne()
+	partTwo()
 }
