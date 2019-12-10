18fa530fc86648144d00184208943a922e50456f
Lion Ralfs <lion.ralfs@gmail.com>
2019-12-10 22:41:36 +0100

    [day10] add part2

--- day10/main.go
+++ day10/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math"
+	"strconv"
 	"strings"
 )
 
@@ -20,8 +21,18 @@ type Field struct {
 	sizeY int
 }
 
+// An Astroid with some info associated with them (relative to a certain position)
+type Astroid struct {
+	coordinates Coordinate
+	// the manhattan distance to the
+	distance  int
+	angle     float64
+	destroyed bool
+}
+
 func main() {
 	partOne()
+	partTwo()
 }
 
 func partOne() {
@@ -29,7 +40,97 @@ func partOne() {
 	field := NewField(string(bytes))
 
 	coord, count := field.SeesMax()
-	fmt.Printf("%v can see %v other astroids\n", coord, count)
+	fmt.Printf("[Part1] %v can see %v other astroids\n", coord, count)
+}
+
+func partTwo() {
+	bytes, _ := ioutil.ReadFile("./input.txt")
+	field := NewField(string(bytes))
+
+	coord, _ := field.SeesMax()
+
+	astroids := make([]*Astroid, 0)
+	for x, col := range field.data {
+		for y, element := range col {
+			if element == "#" {
+				xDifference := x - coord.x
+				yDifference := coord.y - y
+
+				angle := math.Atan2(float64(xDifference), float64(yDifference)) * (180 / math.Pi)
+
+				if angle < 0 {
+					angle = 360 + angle
+				}
+
+				astroid := &Astroid{
+					coordinates: Coordinate{x, y},
+					distance:    int(math.Abs(float64(xDifference)) + math.Abs(float64(yDifference))),
+					angle:       angle, // TODO
+				}
+				astroids = append(astroids, astroid)
+			}
+		}
+	}
+
+	// start at a negative number so we can be sure to hit the 0 angles first
+	var currentAngle float64 = -1.0
+	for i := range astroids {
+		bestTarget := NextTarget(astroids, currentAngle)
+		currentAngle = bestTarget.angle
+		bestTarget.destroyed = true
+
+		// update angles that we've already passed
+		for _, e := range astroids {
+			if e.angle <= currentAngle {
+				e.angle += 360
+			}
+		}
+
+		field.data[bestTarget.coordinates.x][bestTarget.coordinates.y] = strconv.Itoa(i + 1)
+
+		if i+1 == 200 {
+			x := bestTarget.coordinates.x
+			y := bestTarget.coordinates.y
+			fmt.Printf("[Part2] The 200th destroyed astroid is at (x, y): (%v, %v), so the solution is %v\n", x, y, x*100+4)
+			return
+		}
+
+	}
+}
+
+// NextTarget selects the next target to shoot
+func NextTarget(list []*Astroid, oldAngle float64) *Astroid {
+
+	var bestTarget *Astroid = nil
+	for _, astroid := range list {
+		if astroid.destroyed {
+			continue
+		}
+
+		angle := astroid.angle
+		if angle <= oldAngle {
+			// skip angles that we've already passed
+			continue
+		}
+
+		if bestTarget == nil {
+			bestTarget = astroid
+			continue
+		}
+
+		// if we find a smaller angle than the one we already assumed to be the smallest, save it
+		if angle < bestTarget.angle {
+			bestTarget = astroid
+		} else if angle == bestTarget.angle {
+			// in case we found one with the same angle, use the one with the smaller distance
+			if astroid.distance < bestTarget.distance {
+				bestTarget = astroid
+			}
+		}
+
+	}
+
+	return bestTarget
 }
 
 // NewField creates a new field instance from a string
@@ -134,6 +235,16 @@ func (field Field) SeesMax() (Coordinate, int) {
 	}
 
 	return astroid, maxCount
+}
+
+// Print prints the field in a human-readable format
+func (field Field) Print() {
+	for y := 0; y < field.sizeY; y++ {
+		for x := 0; x < field.sizeX; x++ {
+			fmt.Printf("%v ", field.data[x][y])
+		}
+		fmt.Println("")
+	}
 }
 
 func gcd(a, b int) int {
